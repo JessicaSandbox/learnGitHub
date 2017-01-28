98f8f46a64b7048a7b3d868867d8b46c211d1171
Jessica <Jessica>
2017-01-28 01:14:15 -0500

    Created repository from eclipse and making first commit and push

---
+++ src/learnGitHub/LearnGitHub.java
@@ -0,0 +1,11 @@
+package learnGitHub;
+
+public class LearnGitHub {
+
+	public static void main(String[] args) {
+		
+		System.out.println("This program is soley to learn Git and GitHub with eclipse.");
+
+	}
+
+}
