063a7382ae85a31cf23d2817561f6e2ec3ea49f1
Jessica <Jessica>
2017-01-28 01:44:39 -0500

    **Second commit**

--- src/learnGitHub/LearnGitHub.java
+++ src/learnGitHub/LearnGitHub.java
@@ -5,6 +5,8 @@ public class LearnGitHub {
 	public static void main(String[] args) {
 		
 		System.out.println("This program is soley to learn Git and GitHub with eclipse.");
+		System.out.println("**Making a change to the original code**");
+		
 
 	}
 
